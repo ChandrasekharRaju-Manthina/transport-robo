349a8328c946ffbe463f8d99b7d6681fad57b967
Chandra <chandra.javadeveloper@gmail.com>
2015-12-13 21:21:18 +0530

    removed unneccessary files

--- trobo/src/main/java/com/allstate/trobo/dao/SpittleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.allstate.trobo.dao;
-
-import java.util.List;
-
-import com.allstate.trobo.domain.Spittle;
-
-public interface SpittleRepository {
-	List<Spittle> findSpittles(long max, int count);
-}
